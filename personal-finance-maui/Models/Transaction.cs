8d4277f040bb9742b121b384f0150437589b3ebc
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-12-07 17:49:19 -0300

    Apply .gitignore cleanup

---
+++ personal-finance-maui/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿namespace personal_finance_maui.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public decimal Value { get; set; }
+        public TransactionType Type { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expanses
+    }
+}
