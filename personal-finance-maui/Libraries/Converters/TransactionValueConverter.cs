363731ce8309f6a9e169c7ea199c70f5eccc78f1
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-01-04 08:44:55 -0300

    conversion of values ​​according to the type of transaction

---
+++ personal-finance-maui/Libraries/Converters/TransactionValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using personal_finance_maui.Models;
+using System.Globalization;
+
+namespace personal_finance_maui.Libraries.Converters
+{
+    public class TransactionValueConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+
+            if (transaction == null) return string.Empty;
+
+            if(transaction.Type == TransactionType.Income)
+            {
+                return transaction.Value.ToString("C");
+            }else
+            {
+                return $"- {transaction.Value.ToString("C")}";
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
